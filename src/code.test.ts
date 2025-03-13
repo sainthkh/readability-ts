d991d4ff1ebb87a1d9923598baa170c982ae5b81
Kukhyeon Heo <sainthkh@gmail.com>
2025-03-13 20:42:30 +0900

    Fix code block.

--- src/code.test.ts
+++ src/code.test.ts
@@ -14,10 +14,24 @@ describe('tags', () => {
             const expected = await readFile('testcases/add-article.expected.html', 'utf-8');
             const dom = new JSDOM(html);
 
-            const readable = extractReadable(dom.window.document);
+            const readable = extractReadable(dom.window.document, 'all');
             // await writeFile('testcases/add-article.actual.html', readable!, 'utf-8');
 
             expect(readable).toBe(expected);
+        })
+    })
+})
+
+describe('sites', () => {
+    describe('craftinginterpreters.com', () => {
+        test.only('add code blocks correctly', async () => {
+            const html = await readFile('testcases/craftinginterpreters.com/code-block.html', 'utf-8');
+
+            const dom = new JSDOM(html);
+
+            const readable = extractReadable(dom.window.document, 'craftinginterpreters.com');
+            await writeFile('testcases/craftinginterpreters.com/code-block.actual.html', readable!, 'utf-8');
+
         })
     })
 })
--- src/code.ts
+++ src/code.ts
@@ -1,7 +1,37 @@
 export const add = (a: number, b: number) => a + b;
 
-export const extractReadable = (doc: Document) => {
+export const extractReadable = (doc: Document, context: string) => {
     const article = doc.querySelector('article');
+
+    if (context === 'craftinginterpreters.com') {
+        // code block
+        article?.querySelectorAll('.codehilite pre').forEach(pre => {
+            const codeElement = doc.createElement('code');
+            const lines = pre.innerHTML.split('\n');
+            let minIndent = 10000;
+            lines.forEach((line, i) => {
+                if (i === 0) return;
+
+                let count = 0
+                while (line[count] === ' ') {
+                    count++;
+                }
+
+                if (count < minIndent) {
+                    minIndent = count;
+                }
+            })
+            const trimmed =
+                minIndent === 10000
+                    ? lines.join('\n')
+                    : lines.map((line, i) => i === 0 ? line : line.slice(minIndent)).join('\n');
+            codeElement.innerHTML = trimmed;
+            pre.innerHTML = '';
+            pre.appendChild(codeElement);
+        })
+
+        article?.querySelectorAll('footer').forEach(footer => { footer.remove() })
+    }
 
     return article?.innerHTML;
 }
